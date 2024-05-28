4b81df19e23d4fca93f65246255cd053e0149892
Matias Novillo <novillo.matias1@gmail.com>
2024-05-28 10:42:33 -0300

    Se hicieron avances en la app

---
+++ wwwroot/js/BasicCore/Storage.js
@@ -0,0 +1,20 @@
+﻿window.StorageFunctions = {
+    setItem: function (key, value) {
+        localStorage.setItem(key, value);
+    },
+    getItem: function (key) {
+        return localStorage.getItem(key);
+    },
+    removeItem: function (key) {
+        localStorage.removeItem(key);
+    },
+    setSessionItem: function (key, value) {
+        sessionStorage.setItem(key, value);
+    },
+    getSessionItem: function (key) {
+        return sessionStorage.getItem(key);
+    },
+    removeSessionItem: function (key) {
+        sessionStorage.removeItem(key);
+    }
+};
